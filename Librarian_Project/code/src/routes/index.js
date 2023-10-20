eef7f37198f095d1540f527536f9fc112be15a83
GiaBao0510 <pgiabao2002@gmail.com>
2023-10-20 23:02:15 +0700

    capNhat by GiaBao

--- Librarian_Project/code/src/routes/index.js
+++ Librarian_Project/code/src/routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const router = express.Router()
 
-// router.get('', (req,res,next)=>{
+// router.get('/test', (req,res,next)=>{
 //     return res.status(200).json({
 //         message: 'Welcome'
 //     })
