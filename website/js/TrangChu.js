642fa104bfa9ae02a1546520285316d4a87bb02d
GiaBao0510 <pgiabao2002@gmail.com>
2023-10-28 19:42:41 +0700

    update Bao

---
+++ website/js/TrangChu.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',()=>{
+
+    //Thêm sự kiện cho nút
+    // $('#DanhSachTinhTrangSach').click(function (e) { 
+    //     e.preventDefault();
+    //     $('.CotHienThiChucNang').css("height","100%");
+    // });
+});
