70d5b502823bcb490f0ed3320d3237b769476c6c
Đặng Văn Khốm <kendivan123@gmail.com>
2023-11-03 22:07:19 +0700

    Dăng Van Khom
    hehe

--- website/DoiTuongSuDung/Index/Dktk.js
+++ website/DoiTuongSuDung/Index/Dktk.js
@@ -7,12 +7,21 @@ $(document).ready(function() {
 
     registerButton.on('click', function(event) {
         event.preventDefault(); // Ngăn chặn gửi biểu mẫu mặc định
-
-        if (passwordInput.val() !== confirmPasswordInput.val()) {
+        
+        // Kiểm tra các trường input
+        var fullNameInput = $('#full-name');
+        var usernameInput = $('#username');
+        var birthdateInput = $('#birthdate');
+        var phoneInput = $('#phone');
+        var addressInput = $('#address');
+        
+        if (fullNameInput.val() === '' || usernameInput.val() === '' || birthdateInput.val() === '' || phoneInput.val() === '' || addressInput.val() === '') {
+            alert("Vui lòng điền đầy đủ các trường.");
+        } else if (passwordInput.val() !== confirmPasswordInput.val()) {
             passwordError.text("Mật khẩu bạn vừa nhập không khớp.");
         } else {
             passwordError.text("");
-            registrationForm.submit(); // Gửi biểu mẫu nếu mật khẩu khớp
+            registrationForm.submit(); // Gửi biểu mẫu nếu tất cả đều hợp lệ
         }
     });
 });
