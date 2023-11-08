19b80c2117d1e11f11c497f40e6aad49ed4d5aaa
Nhanguyen <nhanguyennhtn@gmai.com>
2023-11-09 01:55:53 +0700

    nha.form chititetsach

---
+++ website/DoiTuongSuDung/DungChung/quanlydangnhap.php
@@ -0,0 +1,29 @@
+<?php
+    include('../../php/ConnectMySQL.php');
+    include('../../php/CacHamXuLy.php');
+    include('../../QTDL/ct467_quanlythuvien/website/');
+    echo "<p> Tài khoản: ".$_POST['UserID']."</p>";
+    echo "<p> Mật khẩu: ".$_POST['MatKhau']."</p>";
+
+    $taiKhoan = trim($_POST['UserID']);
+    $pw = trim($_POST['MatKhau']);
+    $role = trim($_POST['Role']);
+    //Thực hiện tìm kiểm
+    $lenhTim = "SELECT COUNT(*) AS soluong FROM taikhoan WHERE UserID = '$taiKhoan' AND MatKhau = '$pw'" or die(mysqli_connect_error());
+    $thucHien = mysqli_query($connect,$lenhTim) or die(mysqli_connect_error());
+    $ketQua = intval(mysqli_fetch_array($thucHien)['soluong']);
+    $KhongThay = "";
+
+    //Điều kiện nếu tìm thấy thì tìm kiếm xem thuộc về đối tượng nào
+    if($ketQua > 0){
+        if($role === 0) {
+            header('Location: Librarian_Project\code\dist\trangchu.html?ID='.$taiKhoan.'');
+        }elseif ($role === 1) {
+            header('Location: Librarian_Project\code\dist\trangchu.html?ID='.$taiKhoan.'');
+        }else {
+            echo "<p> Không tìm thấy</p>";
+        $KhongThay = "KhongThayTaiKhoan";
+        header('Location: notFound.php');
+        }
+    }
+?>
---
+++ website/DoiTuongSuDung/admin/QuanLySach/ChiTietSach.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kiểm tra sách</title>
+    <link rel="stylesheet" href="../../../css/Sach/sach.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="body-wapper">
+        <div class="topnav">
+            <a class="active" href="#home">Tìm kiếm Sách</a>
+            <div class="search-container">
+                <form action="/action_page.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div>
+        <div class="tabel-database">
+            <table id="customers">
+                <tr>
+                    <<th>UserID/Mã đọc giả</th>
+                        <th>Họ tên</th>
+                        <th>Giới tính</th>
+                        <th>Ngày sinh</th>
+                        <th>Số điện thoại</th>
+                        <th>Email</th>
+                        <th>Địa chỉ</th>
+                        <th>Mật khẩu</th>
+                        <th>Delete</th>
+                        <th>Cập nhật</th>
+                </tr>
+                <tr>
+                    <<td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                </tr>
+                <tr>
+                    <<td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>...</td>
+                </tr>
+
+            </table>
+
+        </div>
+        <!-- <div class="title-form">
+            <form action="">
+                <div class="input-username">
+                    <input type="text" placeholder="truy xuất từ tên sách">
+                    <input type="text" placeholder="truy xuất từ sl sách">
+                </div>
+                <div class="input-username">
+                    <input type="text" placeholder="truy xuất từ tên nhanvien">
+                    <input type="text" placeholder="truy xuất từ sl docgia">
+                </div>
+                <div class="input-username">
+                    <input type="date" >
+                    <input type="date" >
+                </div>
+            </form> 
+        </div> -->
+    </div>
+</body>
+
+</html>
