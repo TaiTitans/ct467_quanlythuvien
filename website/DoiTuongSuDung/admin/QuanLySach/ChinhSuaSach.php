60358deb0154a7bdb036f312ea2a7e6d453f54f8
Phan Phat Tai <taititansofficial@gmail.com>
2023-11-10 00:03:00 +0700

    update

--- website/DoiTuongSuDung/admin/QuanLySach/ChinhSuaSach.php
+++ website/DoiTuongSuDung/admin/QuanLySach/ChinhSuaSach.php
@@ -3,7 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Thêm Sách</title>
+        <title>Chỉnh sửa sách</title>
+        <link href="/Librarian_Project/code/dist/output.css" rel="stylesheet">
+        <link rel="stylesheet" href="/Librarian_Project/code/dist/assets/css/style.css">
+        <link rel="shortcut icon" href="/Librarian_Project/code/dist/assets/img/favicon.png">
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
         <!--Bootstrap-->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
@@ -37,31 +42,129 @@
         ?>  
     </head>
     <body>
-        <header></header>
+    <script>
+  document.addEventListener('DOMContentLoaded', function () {
+  const userMenuButton = document.getElementById('user-menu-button');
+  const userMenu = document.querySelector('.user-menu');
+
+      userMenuButton.addEventListener('click', function() {
+        userMenu.classList.toggle('hidden');
+      });
+    });
+  </script>
+  <div class="header">
+    <nav class="bg-gray-800">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <!-- Mobile menu button-->
+            <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+              <span class="absolute -inset-0.5"></span>
+              <span class="sr-only">Mở menu</span>
+              <!--
+                      Icon when menu is closed.
+          
+                      Menu open: "hidden", Menu closed: "block"
+                    -->
+              <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+              <!--
+                      Icon when menu is open.
+          
+                      Menu open: "block", Menu closed: "hidden"
+                    -->
+                    <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div class="flex flex-shrink-0 items-center">
+                    <img class="h-8 w-auto"  src="/Librarian_Project/code/dist/assets/img/favicon.png" alt="logo">
+                  </div>
+                  <div class="hidden sm:ml-6 sm:block">
+                    <div class="flex space-x-4">
+                      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                      <a href="../../../Librarian_Project/code/dist/trangchu.html" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Trang chủ</a>
+                      <a href="../../website/DoiTuongSuDung/admin/QuanLySach/DanhSachSach.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Kho Sách</a>
+                      <a href="../../website/DoiTuongSuDung/admin/QuanLyNhanVien/DanhSachNhanVien.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Nhân Viên</a>
+                      <a href="../../website/DoiTuongSuDung/admin/QuanLyDocGia/DanhSachDocGia.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Thông tin cho mượn</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span class="absolute -inset-1.5"></span>
+                    <span class="sr-only">Xem thông báo</span>
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                    </svg>
+                  </button>
+          
+                  <!-- Profile dropdown -->
+                  <div class="relative ml-3">
+                    <div>
+                      <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <span class="absolute -inset-1.5"></span>
+                        <span class="sr-only">Mở menu</span>
+                        <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                      </button>
+                    </div>
+        
+                    <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none user-menu hidden" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                      <!-- Active: "bg-gray-100", Not Active: "" -->
+                      <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Đăng xuất</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+            <!-- Mobile menu, show/hide based on menu state. -->
+            <div class="sm:hidden" id="mobile-menu">
+              <div class="space-y-1 px-2 pb-3 pt-2">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Trang chủ</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Kho Sách</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Nhân Viên</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Thông Tin Cho Mượn</a>
+              </div>
+            </div>
+          </nav>
+    </div>
+        </div>
         <main>
-            <div class="">
-                <form name="BieuMauThemSach" action="ThucHienThemSach.php" method="post" enctype="application/x-www-form-urlencoded" onsubmit="return BieuMauThemSach()">
-                    <table>
-                        <tr>
-                            <td>
-                                <p>ID sách:</p>
-                                <input type="text" name="idSach" value="<?php echo $TTsach['idSach'];?>">
-                            </td>
-                            <td>
-                                <p>Tên sách:</p>
-                                <input type="text" name="tenSach" value="<?php echo $TTsach['tenSach'];?>">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p>Năm xuất bản:</p>
-                                <input type="text" name="namXuatBan" value="<?php echo $TTsach['namXuatBan'];?>">
-                            </td>
-                            <td>
-                                <p>Nhà xuất bản:</p>
-                                <?php
+        <div class="max-w-2xl mx-auto mt-10 mb-10 bg-white shadow-lg rounded-lg overflow-hidden">
+    <div class="text-2xl py-4 px-6 bg-gray-900 text-white text-center font-bold uppercase">
+        Chỉnh sửa sách
+    </div>
+    <form class="py-4 px-6" action="ThucHienThemSach.php" method="post" name="BieuMauThemSach" enctype="application/x-www-form-urlencoded" onsubmit="return BieuMauThemSach()">
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="name">
+                ID Sách
+            </label>
+            <input type="text" name="idSach" value="<?php echo $TTsach['idSach'];?>">
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="email">
+                Tên sách
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="tenSach" value="<?php echo $TTsach['tenSach'];?>">
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="phone">
+                Năm xuất bản
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="namXuatBan" value="<?php echo $TTsach['namXuatBan'];?>">
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="service">
+                Nhà xuất bản
+            </label>
+            <?php
                                     mysqli_next_result($connect);
-                                    echo '<select name="idNXB" id="">';
+                                    echo '<select name="idNXB" id="" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">';
                                     $LayThongTinNXB = "SELECT * FROM NhaXuatBan";
                                     $ThucHienLay = TruyVan($LayThongTinNXB);
                                     while($row = mysqli_fetch_array($ThucHienLay)){
@@ -73,19 +176,19 @@
                                     }
                                     echo '</select>';
                                 ?>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p>Tác giả:  </p>
-                                <?php
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="service">
+                Tác giả
+            </label>
+            <?php
                                     mysqli_next_result($connect);
                                     
                                     $LayTacGia = "SELECT * FROM tacgia";
                                     $act_LayTacGia = TruyVan($LayTacGia);
                                     mysqli_next_result($connect);
 
-                                    echo '<select name="IDTacGia">';
+                                    echo '<select name="IDTacGia" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">';
 
                                     while($row = mysqli_fetch_array($act_LayTacGia)){
                                         mysqli_next_result($connect);
@@ -99,11 +202,13 @@
                                     }
                                     echo '</select>';
                                 ?>
-                            </td>
-                            <td>
-                                <p>Thể loại:</p>
-                                <?php
-                                    echo '<select name="idTheLoai" id="">';
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="service">
+                Thể loại
+            </label>
+            <?php
+                                    echo '<select name="idTheLoai" id="" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">';
                                     $LayThongTinTheLoai = "SELECT * FROM TheLoai";
                                     $ThucHienLay = TruyVan($LayThongTinTheLoai);
                                     while($row = mysqli_fetch_array($ThucHienLay)){
@@ -119,6 +224,25 @@
                                     echo '</select>';
                                     
                                 ?>
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="message">
+                Mô tả
+            </label>
+            <textarea
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="message" rows="4" name="MoTa"><?php echo $TTsach['MoTa'];?></textarea>
+        </div>
+        <div class="flex items-center justify-center">
+            <button
+                class="bg-gray-900 text-white py-2 px-4 rounded hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+                type="submit">
+                Lưu Lại
+            </button>
+        </div>
+
+    </form>
+</div>
                             </td>
                         </tr>
                         <tr>
